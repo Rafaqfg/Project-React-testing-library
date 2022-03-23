ae20699b556f54ca0c5bf467c23992069209816a
Rafael Godoy <rafa.qfg@gmail.com>
2022-03-23 09:34:40 -0300

    finaliza testes do arquivo App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>),
+    ...render(<Router history={history}>{component}</Router>),
   });
   // função para contornar o problema do history do Router retirada do course: https://app.betrybe.com/course/front-end/testes-automatizados-com-react-testing-library/rtl-testando-react-router/58c480e0-79ed-47bd-a819-f88d82997927/conteudos/0189511f-5c08-4bea-9c72-0cecefb24011/testando-react-router/81d72a46-fc66-48e1-a69b-d8f3a0388424?use_case=side_bar
 };
@@ -15,23 +16,23 @@ const renderWithRouter = (component) => {
 describe(
   'Testa se o componente <App.js /> exibe corretamente a barra de navegação com os links',
   () => {
-    test('testa se a aplicação contém um conjunto de links de navegação em tag `<nav>`',
+    test('1.testa se a aplicação contém um conjunto de links de navegação em tag `<nav>`',
       () => {
         renderWithRouter(<App />);
         const navBar = screen.getByRole('navigation');
         expect(navBar).toBeInTheDocument();
       });
-    it('testa se o primeiro link possui o texto `Home`', () => {
+    it('2.testa se um link possui o texto `Home`', () => {
       renderWithRouter(<App />);
       const homeLink = screen.getByText(/Home/i);
       expect(homeLink).toBeInTheDocument();
     });
-    it('testa se o segundo link possui o texto `About`', () => {
+    it('3.testa se um link possui o texto `About`', () => {
       renderWithRouter(<App />);
       const aboutLink = screen.getByText(/About/i);
       expect(aboutLink).toBeInTheDocument();
     });
-    it('testa se o terceiro link possui o texto `Favorite Pokémons`', () => {
+    it('4.testa se um link possui o texto `Favorite Pokémons`', () => {
       renderWithRouter(<App />);
       const favoritePokemonsLink = screen.getByText(/Favorite Pokémons/i);
       expect(favoritePokemonsLink).toBeInTheDocument();
@@ -40,27 +41,51 @@ describe(
 );
 describe('Testa se os links do componente `<App.js />` funcionam corretamente', () => {
   it(
-    'testa se a aplicação é redirecionada para a página inicial ao clicar no link `Home`',
+    '1.testa se a aplicação é redirecionada à página inicial ao clicar no link `Home`',
     () => {
-      fail();
+      renderWithRouter(<App />);
+      const homeLink = screen.getByText(/Home/i);
+      userEvent.click(homeLink);
+      const title = screen
+        .getByRole('heading', { level: 2, name: /Encountered pokémons/i });
+      expect(title).toBeInTheDocument();
     },
   );
   it(
-    'testa se a aplicação é redirecionada para a página About ao clicar no link `About`',
+    '2.testa se a aplicação é redirecionada à página About ao clicar no link `About`',
     () => {
-      fail();
+      renderWithRouter(<App />);
+      const aboutLink = screen.getByText(/About/i);
+      userEvent.click(aboutLink);
+      const title = screen
+        .getByRole('heading', { level: 2, name: /About Pokédex/i });
+      expect(title).toBeInTheDocument();
     },
   );
   it(
-    'testa se a aplicação é redirecionada para a pág Pokémons Favoritados em favorites',
+    '3.testa se a aplicação é redirecionada à página Pokémons Favoritados em favorites',
     () => {
-      fail();
+      renderWithRouter(<App />);
+      const favoritePokemonsLink = screen.getByText(/Favorite Pokémons/i);
+      userEvent.click(favoritePokemonsLink);
+      const title = screen
+        .getByRole('heading', { level: 2, name: /Favorite pokémons/i });
+      expect(title).toBeInTheDocument();
     },
   );
   it(
-    'testa se a aplicação é redirecionada à pág Not Found ao entrar em url desconhecida',
+    '4.testa se a aplicação é redirecionada à pág NotFound ao entrar em url desconhecida',
     () => {
-      fail();
+      const customHistory = createMemoryHistory();
+      render(
+        <Router history={ customHistory }>
+          <App />
+        </Router>,
+      );
+      customHistory.push('/xablau');
+      const title = screen
+        .getByRole('heading', { level: 2, name: /Page requested not found/i });
+      expect(title).toBeInTheDocument();
     },
   );
 });
