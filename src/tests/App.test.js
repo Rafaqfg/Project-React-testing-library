e6c590f66deb1011bc7d3a729906f9c12ae39753
Rafael Godoy <rafa.qfg@gmail.com>
2022-03-23 10:26:23 -0300

    correcao lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,7 +8,7 @@ import App from '../App';
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={history}>{component}</Router>),
+    ...render(<Router history={ history }>{component}</Router>),
   });
   // função para contornar o problema do history do Router retirada do course: https://app.betrybe.com/course/front-end/testes-automatizados-com-react-testing-library/rtl-testando-react-router/58c480e0-79ed-47bd-a819-f88d82997927/conteudos/0189511f-5c08-4bea-9c72-0cecefb24011/testando-react-router/81d72a46-fc66-48e1-a69b-d8f3a0388424?use_case=side_bar
 };
