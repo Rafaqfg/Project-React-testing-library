2e05139ffe8705785fd52af7b0d2f824dcf2d226
Rafael Godoy <rafa.qfg@gmail.com>
2022-03-23 20:12:16 -0300

    cria função everyCheck

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 
 export const renderWithRouter = (component) => {
@@ -9,4 +10,16 @@ export const renderWithRouter = (component) => {
     ...render(<Router history={ history }>{component}</Router>),
   });
   // função para contornar o problema do history do Router retirada do course: https://app.betrybe.com/course/front-end/testes-automatizados-com-react-testing-library/rtl-testando-react-router/58c480e0-79ed-47bd-a819-f88d82997927/conteudos/0189511f-5c08-4bea-9c72-0cecefb24011/testando-react-router/81d72a46-fc66-48e1-a69b-d8f3a0388424?use_case=side_bar
+};
+
+export const everyCheck = (nextPokemonBtn, arr) => {
+  arr.every((pokemon, index) => {
+    if (arr[index] === 0) return true;
+    userEvent.click(nextPokemonBtn);
+    const currentPokemon = screen.getByTestId('pokemon-name');
+    const result = expect(currentPokemon).toHaveTextContent(pokemon.name);
+    if (result) return true;
+    return false;
+    
+  });
 };
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
-import { renderWithRouter } from '../services/helpers';
+import { renderWithRouter, everyCheck } from '../services/helpers';
 import App from '../App';
-import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+import { Pokedex } from '../components';
 
 describe('1 - Testa o componente `<Pokedex.js />`', () => {
   it('1.1 Testa se página contém um heading h2 com o texto Encountered pokémons', () => {
@@ -22,11 +22,18 @@ describe('1 - Testa o componente `<Pokedex.js />`', () => {
 
 describe('2 - Testa a funcionalidade do botão `Próximo pokémon`', () => {
   it('2.1 O botão deve conter o texto `Próximo pokémon`', () => {
-    fail()
+    renderWithRouter(<App />);
+    const nextPokemonBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPokemonBtn).toBeDefined();
   });
   it('2.2 Os próximos Pokémons da lista devem ser mostrados, um a um, ao clicar no botão',
     () => {
-      fail()
+      renderWithRouter(<Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ {} }
+      />);
+      const nextPokemonBtn = screen.getByTestId('next-pokemon');
+      expect(everyCheck(nextPokemonBtn, pokemons)).toBeTruthy();
     });
   it('2.3 O 1o Pokémon deve ser mostrado ao clicar no botão,se estiver no último Pokémon',
     () => {
@@ -38,38 +45,38 @@ describe('2 - Testa a funcionalidade do botão `Próximo pokémon`', () => {
     });
 });
 
-describe('3 - Testa a funcionalidade dos botões de filtro', () => {
-  it('3.1 Testa se a Pokédex tem os botões de filtro', () => {
-    fail()
-  });
-  it('3.2 Deve existir um botão de filtragem para cada tipo de Pokémon, sem repetição',
-    () => {
-      fail()
-    });
-  it('3.3 A partir da seleção de 1 tipo, deve circular somente pelos pokémons deste tipo',
-    () => {
-      fail()
-    });
-  it('3.4 O texto do botão deve corresponder ao nome do tipo, `ex. Psychic`', () => {
-    fail()
-  });
-  it('3.5 O botão `All` precisa estar sempre visível', () => {
-    fail()
-  });
-});
+// describe('3 - Testa a funcionalidade dos botões de filtro', () => {
+//   it('3.1 Testa se a Pokédex tem os botões de filtro', () => {
+//     fail()
+//   });
+//   it('3.2 Deve existir um botão de filtragem para cada tipo de Pokémon, sem repetição',
+//     () => {
+//       fail()
+//     });
+//   it('3.3 A partir da seleção de 1 tipo, deve circular somente pelos pokémons deste tipo',
+//     () => {
+//       fail()
+//     });
+//   it('3.4 O texto do botão deve corresponder ao nome do tipo, `ex. Psychic`', () => {
+//     fail()
+//   });
+//   it('3.5 O botão `All` precisa estar sempre visível', () => {
+//     fail()
+//   });
+// });
 
-describe('4 - Testa a funcionalidade do botão de resetar os filtros', () => {
-  it('4.1 Testa se a Pokédex contém um botão para resetar os filtros', () => {
-    fail()
-  });
-  it('4.2 O texto do botão deve ser `All`', () => {
-    fail()
-  });
-  it('4.3 A Pokedéx mostra os Pokémons normalmente, quando o botão `All` for clicado',
-    () => {
-      fail()
-    });
-  it('4.4 Ao carregar a página, o filtro selecionado deverá ser `All`', () => {
-    fail()
-  });
-});
+// describe('4 - Testa a funcionalidade do botão de resetar os filtros', () => {
+//   it('4.1 Testa se a Pokédex contém um botão para resetar os filtros', () => {
+//     fail()
+//   });
+//   it('4.2 O texto do botão deve ser `All`', () => {
+//     fail()
+//   });
+//   it('4.3 A Pokedéx mostra os Pokémons normalmente, quando o botão `All` for clicado',
+//     () => {
+//       fail()
+//     });
+//   it('4.4 Ao carregar a página, o filtro selecionado deverá ser `All`', () => {
+//     fail()
+//   });
+// });
