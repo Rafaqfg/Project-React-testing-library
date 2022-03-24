b1ef903931bcc8527e35d7b2c79da73a641c390f
Rafael Godoy <rafa.qfg@gmail.com>
2022-03-24 18:40:27 -0300

    finaliza testes componente <Pokemon />

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,6 +5,10 @@ import { Router } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
+const HREF = '/pokemons/25';
+const PIKACHU_URL = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+const STAR_IMG = '/star-icon.svg';
+
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
@@ -18,15 +22,36 @@ describe('1 - Testa o componente `<Pokemon />`', () => {
       renderWithRouter(<App />);
       const pokemonName = screen.getByText(/pikachu/i);
       const pokemonImg = screen.getByRole('img', { name: /pikachu sprite/i });
+      const pokemonType = screen.getByTestId('pokemon-type');
+      expect(pokemonType).toHaveTextContent(/electric/i);
+      expect(pokemonImg).toHaveAttribute('src', PIKACHU_URL);
       expect(pokemonName).toBeDefined();
       expect(pokemonImg).toBeInTheDocument();
     });
 
-  it('Teste se o card do Pokémon Pokédex contém um link para exibir mais detalhes.',
+  it('1.2Testa se o card do Pokémon na Pokédex contém um link para exibir mais detalhes.',
     () => {
       renderWithRouter(<App />);
       const moreDetails = screen.getByRole('link', { name: /more details/i });
-      const hRef = '/pokemons/25';
-      expect(moreDetails).toHaveAttribute('href', hRef);
+      expect(moreDetails).toHaveAttribute('href', HREF);
     });
+
+  it('1.3 Testa se ao clicar no link é'
+  + 'feito o redirecionamento para a página de detalhes do Pokémon', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const pokemonDetails = screen.getByRole('heading', { name: /pikachu details/i });
+    expect(pokemonDetails).toBeInTheDocument();
+  });
+
+  it('1.4 Testa se existe um ícone de estrela nos Pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const favoriteCheckBox = screen.getByLabelText(/pokémon favoritado?/i);
+    userEvent.click(favoriteCheckBox);
+    const starIcon = screen.getByAltText(/is marked as favorite/i);
+    expect(starIcon).toHaveAttribute('src', STAR_IMG);
+  });
 });
